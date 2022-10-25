e5bd4c4f029ffd70bca9ff908b6080da3a4741c3
Павел Лукьянов <pahan-01.04.10@mail.ru>
2022-10-25 18:01:19 +0500

    add sections

--- js/main.js
+++ js/main.js
@@ -25,4 +25,34 @@ document.addEventListener('DOMContentLoaded', function () {
             el: '.swiper-scrollbar',
         },
     });
+
+
+    //Галерея загрузить еще
+    let photoMore = document.getElementById('photo-more');
+    photoMore.addEventListener('click', function () {
+        let invisibles = document.querySelectorAll('.photogalery__invisible');
+        invisibles.forEach(invisible => {
+            invisible.classList.toggle('active');
+        });
+        if (photoMore.textContent.trim() === 'Загрузить ещё') {
+            photoMore.textContent = 'Скрыть';
+        } else {
+            photoMore.textContent = 'Загрузить ещё'
+        }
+    })
+
+
+    //Карта
+    var myMap;
+    // Дождёмся загрузки API и готовности DOM.
+    ymaps.ready(init);
+
+    function init() {
+        myMap = new ymaps.Map('map', {
+            center: [55.76, 37.64], // Москва
+            zoom: 10
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
+    }
 });
